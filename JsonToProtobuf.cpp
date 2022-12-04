30a2e0ca4bc86181ec5a2af7405f5b0bdd8bc387
seththorup <seth.thorup@me.com>
2022-12-03 23:53:26 -0700

    updated to more generic robust reading of config file parameters.

--- JsonToProtobuf.cpp
+++ JsonToProtobuf.cpp
@@ -29,28 +29,37 @@ bool jsontoprotobuf::convert_json_to_protobuf(const string &json_file_name,
                                               Message &proto_msg) {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-  bool status{true};
+  if (!std::filesystem::exists(json_file_name)) {
+    string error_report{"Error: file does not exsist " + json_file_name};
+    throw(error_report);
+    return false;
+  }
+
+  ptree json_file_ptree;
+  if (read_json(json_file_name, json_file_ptree); json_file_ptree.empty()) {
+    throw("File failed to load properly");
+    return false;
+  }
+
+  // purpose built function to parse linkbudget config file data_links_list
+  auto ptree = json_file_ptree.get_child_optional(object_name);
+  if (ptree.get_ptr() == nullptr) {
+    throw("Child not found");
+    return false;
+  }
 
   try {
-    ptree root;
-    read_json(json_file_name, root);
+    std::ostringstream msg_oss;
+    write_json(msg_oss, ptree.value());
+    auto json_string = msg_oss.str();
 
-    JsonParseOptions options;
-
-    // get data_link protobuf values from json
-    auto data_link_ptree = root.get_child(object_name);
-
-    std::ostringstream data_link_oss;
-    write_json(data_link_oss, data_link_ptree);
-    string json_string = data_link_oss.str();
-
+    google::protobuf::util::JsonParseOptions options;
     JsonStringToMessage(json_string, &proto_msg, options);
   } catch (exception &ex) {
-    cout << "failed convert json file " << json_file_name << " using "
-         << object_name << " " << __FUNCTION__ << endl;
-    status = false;
+    cout << __FUNCTION__ << " error in processing ptree! " << ex.what() << endl;
+    return false;
   }
-  return status;
+  return true;
 }
 
 bool jsontoprotobuf::convert_protobuf_to_json(const Message &proto_msg,
